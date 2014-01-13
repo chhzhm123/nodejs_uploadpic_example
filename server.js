b919142953ae8eabc9fc507cea134a939d739eba
kmfish <lijun_zh@live.cn>
2014-01-13 17:31:18 +0800

    modify port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ function start(route, handle) {
         route(handle, pathname, response, request);
     } 
     
-    http.createServer(onRequest).listen(8888); 
+    http.createServer(onRequest).listen(3000); 
     console.log("Server has started."); 
 } 
 
